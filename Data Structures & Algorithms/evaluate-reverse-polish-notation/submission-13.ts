f6c8c87422b58f871ff76e880f52f9d7f227958c
Eson H <19144304+esonhey@users.noreply.github.com>
2026-08-19 22:10:35 +0200

    Add: evaluate-reverse-polish-notation - submission-13

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-13.ts
@@ -0,0 +1,32 @@
+class Solution {
+    add = (a, b) => a + b
+    sub = (a, b) => a - b
+    mul = (a, b) => a * b
+    div = (a, b) => Math.trunc(a / b)
+
+    operands = {
+        '+': this.add,
+        '-': this.sub,
+        '*': this.mul,
+        '/': this.div
+    }
+
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens: string[]): number {
+        const stack = []
+        const operandsSet = new Set(Object.keys(this.operands))
+        tokens.forEach(c => {
+            if (operandsSet.has(c)) {
+                const b = stack.pop()
+                const a = stack.pop()
+                stack.push(this.operands[c](a, b))
+            } else {
+                stack.push(parseInt(c))
+            }
+        })
+        return stack[0]
+    }
+}
