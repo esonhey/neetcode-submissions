780e42fc7d3a56c10ec7b5b6b90243773b44c137
Eson H <19144304+esonhey@users.noreply.github.com>
2026-08-27 20:22:11 +0200

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    isHappy(n: number): boolean {
+        const cyclicSet = new Set()
+
+        function cycle(n: number): number {
+            return n.toString()
+                .split('')
+                .map(s => Number(s))
+                .reduce((acc, n) => acc + (n * n), 0)
+        }
+        while (true) {
+            if (cyclicSet.has(n)) return false
+            cyclicSet.add(n)
+            n = cycle(n)
+            if (n === 1) return true
+        }
+    }
+}
