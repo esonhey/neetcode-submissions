ad0b620c103d3fd9e9fd75244eae2938fb882fc8
Eson H <19144304+esonhey@users.noreply.github.com>
2026-09-03 18:44:47 +0200

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.ts
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums: number[]): number {
+        let result = nums[0]
+        for (let i = 1; i < nums.length; i++) {
+            result ^= nums[i]
+        }
+        return result
+    }
+}
